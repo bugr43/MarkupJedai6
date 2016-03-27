8f98abe8b578ebce6982fc8b98b09f18b68a5e86
Николай Острижний <bugr43@gmail.com>
2016-03-28 00:26:04 +0300

    5/11 complete

--- app/js/common.js
+++ app/js/common.js
@@ -18,8 +18,34 @@ $(function() {
 	$(".next").click(function(){
 		owl.trigger("next.owl.carousel");
 	});
+
 	$(".prev").click(function(){
 		owl.trigger("prev.owl.carousel");
+	});
+
+	$(".sf-menu").after('<div id="my-menu">');
+	$(".sf-menu").clone().appendTo("#my-menu");
+	$("#my-menu").find('*').attr('style', '');
+	$("#my-menu").find('ul').removeClass('sf-menu');
+	$("#my-menu").mmenu({
+		extensions: ['widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: "Меню"
+		}
+	});
+
+	var api = $("#my-menu").data("mmenu");
+	api.bind('closed', function(){
+		$('.toggle-mnu').removeClass('on');
+	});
+
+	$(".mobile-mnu").click(function() {
+		var mmAPI = $("#my-menu").data( "mmenu"),
+			that = $(this).find(".toggle-mnu");
+		that.toggleClass("on");
+		$(".main-mnu").slideToggle();
+		mmAPI.open();
+		return false;
 	});
 
 	//SVG Fallback
